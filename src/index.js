4d180ebde7df87c73d79a03e3c37af3d35ed46ac
Tom <tomaptx@users.noreply.github.com>
2022-03-30 10:07:46 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+//Here we're importing items we'll need. You can add other imports here.
+
+var table;
+
+// exposing loadData to FileMaker Script
+window.loadData = function (json) {
+
+// TODO: Load the DataTables libraries by linking to the DataTables CDN.
+  //TODO: Get data from FileMaker
+//TODO: prep it for the JS.
+  
+  
+  //TODO: create the columns.
+
+
+  //TODO: Explore options of Datatables
+  //https://datatables.net/reference/option/
+  //https://datatables.net/examples/index
+  // Create the DataTable, after destroying it if already exists
+  table = $("#dtable").DataTable({
+    paging: true,
+    pageLength: 20,
+    searching: true,
+    colReorder: true,
+    columns: [],
+    data: [],
+  });
+
